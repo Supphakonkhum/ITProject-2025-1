acccf8b831b63b8275203207258520af2a094667
suppakon kummanee <vorgolacool@gmail.com>
2025-08-01 05:59:39 +0700

    Add Angular application with routing and page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CourseComponent } from './pages/course/course.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { TrainerComponent } from './pages/trainer/trainer.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CourseComponent,
+    ServiceComponent,
+    TrainerComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
